4c1ccf24ba46db80d3ead37caeadb4d2b0a2a169
Steven Haddox <steven@haddox.us>
2014-12-22 08:59:47 -0500

    Fails when no DN detected

--- spec/omniauth/strategies/casport_integrations_spec.rb
+++ spec/omniauth/strategies/casport_integrations_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 class MockCasport; end
 
-describe OmniAuth::Strategies::Casport do
+describe OmniAuth::Strategies::Casport, type: :strategy do
   attr_accessor :app
-  let(:auth_hash){ last_response.headers['env']['omniauth.auth'] }
-  let(:casport_hash){ last_response.headers['env']['omniauth.casport'] }
+  let(:auth_hash)    { last_response.headers['env']['omniauth.auth'] }
+  let(:casport_hash) { last_response.headers['env']['omniauth.casport'] }
+  let!(:user_cert)   { File.read('spec/certs/ruby_user.crt') }
 
   # customize rack app for testing, if block is given, reverts to default
   # rack app after testing is done
@@ -24,23 +25,58 @@ describe OmniAuth::Strategies::Casport do
   end
 
   before(:all) do
-    set_app!
+    defaults={
+      cas_server: 'https://casport.dev',
+      authentication_path: '/users'
+    }
+    set_app!(defaults)
   end
 
-  describe '#setup_phase' do
-    it 'should access the cert from HTTP_SSL_CLIENT_CERT' do
+#  describe '#setup_phase' do
+#    it 'should log that setup_phase was invoked' do
+#      pending 'FIXME!'
+#      header 'Ssl-Client-Cert', user_cert
+#      get '/auth/casport/setup'
+#    end
+#  end
 
-      get '/auth/casport/setup'
-ap last_response
-      expect(last_response.body).to be_include("CERTSSSSS!!!")
+  describe '#request_phase', :focus do
+    it 'should fail without a client DN' do
+      expect { get '/auth/casport' }.to raise_error(OmniAuth::Error, 'You need a valid DN to authenticate.')
     end
-  end
-  describe '#request_phase' do
-    it 'should assign the DN from HTTP_SSL_CLIENT_S_DN' do
 
+    it 'should assign the user certificate to an omniauth variable' do
+      header 'Ssl-Client-Cert', user_cert
       get '/auth/casport'
-ap last_response
-      expect(last_response.body).to be_include("<form")
+puts last_response.to_yaml
+ap last_request.env
+      expect(last_request.env['HTTP_SSL_CLIENT_CERT']).to eq(user_cert)
+      expect(last_response.headers['env']).to be(!nil)
+#      expect(last_response.session).to be(nil)
+#      expect(last_response.body).to be_include("CERTSSSSS!!!")
+    end
+
+    it 'should request data from the cas_server' do
+      pending 'todo'
     end
   end
+
+  describe '#callback_phase' do
+    context 'success' do
+      it 'should return an omniauth auth_hash' do
+        pending 'todo'
+        get '/auth/casport/callback'
+        expect(last_request.env['omniauth.auth']).to be_kind_of(Hash)
+      end
+    end
+    context 'fail' do
+      pending 'todo'
+    end
+  end
+
+#    expect(last_request.env['rack.session'][:casport][:dn]).to eq(cert_dn)
+#    expect(last_request.env['rack.session'][:casport][:issuer]).to eq(cert_issuer)
+#    expect(last_request.env['rack.session'][:casport][:sid]).to eq(cert_sid)
+#    expect(last_response.status).to eq(200)
+
 end
