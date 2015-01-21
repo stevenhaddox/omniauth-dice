89a3223ee0172df689b93a3211e132966a08d569
Steven Haddox <steven@haddox.us>
2015-01-21 06:09:18 -0500

    auth_hash v1

--- spec/omniauth/strategies/dice_integrations_spec.rb
+++ spec/omniauth/strategies/dice_integrations_spec.rb
@@ -4,22 +4,51 @@ require'dnc'
 class MockDice; end
 describe OmniAuth::Strategies::Dice, type: :strategy do
   attr_accessor :app
-  let(:auth_hash)  { last_response.headers['env']['omniauth.auth'] }
-  let(:dice_hash)  { last_response.headers['env']['omniauth.dice'] }
-  let!(:user_cert) { File.read('spec/certs/ruby_user.crt') }
-  let!(:raw_dn)    { '/DC=org/DC=ruby-lang/CN=Ruby certificate rbcert' }
-  let!(:user_dn)   { DN.new(dn_string: '/DC=org/DC=ruby-lang/CN=Ruby certificate rbcert') }
-  let(:raw_issuer_dn)   { '/DC=org/DC=ruby-lang/CN=Ruby CA' }
-  let(:issuer_dn)   { 'CN=RUBY CA,DC=RUBY-LANG,DC=ORG' }
-  let!(:auth_hash) { {
-    'uid' => 'RUBY CERTIFICATE RBCERT',
-    'email' => '',
-    'extra' => {
-      'info' => {
-        'dn' => user_dn
+  let(:auth_hash)        { full_auth_hash }
+  let!(:user_cert)       { File.read('spec/certs/ruby_user.crt') }
+  let!(:raw_dn)          { '/DC=org/DC=ruby-lang/CN=Ruby certificate rbcert' }
+  let!(:user_dn)         { DN.new(dn_string: '/DC=org/DC=ruby-lang/CN=Ruby certificate rbcert') }
+  let(:raw_issuer_dn)    { '/DC=org/DC=ruby-lang/CN=Ruby CA' }
+  let(:issuer_dn)        { 'CN=RUBY CA,DC=RUBY-LANG,DC=ORG' }
+  let!(:valid_user_json) { File.read('spec/fixtures/valid_auth.json') }
+  let(:valid_user_xml)   { File.read('spec/fixtures/valid_auth.xml') }
+
+  def full_auth_hash
+    {
+      "provider"=>"Dice",
+      "uid"=>"cn=ruby certificate rbcert,dc=ruby-lang,dc=org",
+      "extra" => {
+        "raw_info" => valid_user_json
+      },
+      "info" => {
+        "dn" => "cn=twilight.sparkle,ou=c001,ou=mlp,ou=pny,o=princesses of celestia,c=us",
+        "email" => "twilight@example.org",
+        "first_name" => "twilight",
+        "last_name" => "sparkle",
+        "full_name" => "twilight sparkle",
+        "citizenship_status" => "US",
+        "country" => "USA",
+        "grant_by" => [
+          "princess celestia"
+        ],
+        "organizations" => [
+          "princesses",
+          "librarians",
+          "unicorns"
+        ],
+        "uid" => "twilight.sparkle",
+        "dutyorg" => "ponyville library",
+        "visas" => [
+          "EQUESTRIA",
+          "CLOUDSDALE"
+        ],
+        "affiliations" => [
+          "WONDERBOLTS"
+        ],
+        "telephone_number" => "555-555-5555"
       }
     }
-  } }
+  end
 
   # customize rack app for testing, if block is given, reverts to default
   # rack app after testing is done
@@ -36,17 +65,17 @@ describe OmniAuth::Strategies::Dice, type: :strategy do
       yield
       self.app = old_app
     end
-    Capybara.app = self.app
     self.app
   end
 
   before(:all) do
     defaults={
       cas_server: 'http://example.org',
-      authentication_path: '/users'
+      authentication_path: '/dn'
     }
     set_app!(defaults)
   end
+
 
   describe '#request_phase' do
     it 'should fail without a client DN' do
@@ -85,35 +114,79 @@ describe OmniAuth::Strategies::Dice, type: :strategy do
   end
 
   describe '#callback_phase' do
-    it 'should request data from the cas_server' do
-      header 'Ssl-Client-Cert', user_cert
-      get '/auth/dice'
-      follow_redirect!
-      expect(last_response.location).to eq('/')
-#      ap last_response
-#      ap last_request.env
-#      expect(last_response.env['omniauth.auth']).to eq(auth_hash)
+    before(:each) do
+      set_app!({
+        cas_server:          'https://example.org:3000',
+        authentication_path: '/dn',
+        dnc_options: { transformation: 'downcase' },
+        ssl_config:  {
+          ca_file:     'spec/certs/CA.pem',
+          client_cert: 'spec/certs/client.pem',
+          client_key:  'spec/certs/key.np.pem'
+        }
+      })
+
+      stub_request(:get, "https://example.org:3000/dn/cn=ruby%20certificate%20rbcert,dc=ruby-lang,dc=org/info.json?issuerDN=cn=ruby%20ca,dc=ruby-lang,dc=org").
+      with(:headers => {'Accept'=>'application/json', 'Content-Type'=>'application/json', 'Host'=>'example.org:3000', 'User-Agent'=>'Faraday via Ruby 2.1.5', 'X-Xsrf-Useprotection'=>'false'}).
+      to_return(status: 200, body: valid_user_json, headers: {})
     end
 
     context 'success' do
+      it 'should return a 200 with a JSON object of user information on success' do
+        header 'Ssl-Client-Cert', user_cert
+        get '/auth/dice'
+        follow_redirect!
+        expect(last_response.location).to eq('/')
+        raw_info = last_request.env['rack.session']['omniauth.auth']['extra']['raw_info']
+        expect(raw_info).to eq(valid_user_json)
+      end
+
       it 'should return an omniauth auth_hash' do
         header 'Ssl-Client-Cert', user_cert
         get '/auth/dice'
         follow_redirect!
         expect(last_response.location).to eq('/')
-        expect(last_request.env['omniauth.auth']).to be_kind_of(Hash)
-        expect(last_request.env['omniauth.auth']).to eq(auth_hash)
+        raw_info = last_request.env['rack.session']['omniauth.auth']['extra']['raw_info']
+        expect(last_request.env['rack.session']['omniauth.auth']).to be_kind_of(Hash)
+        expect(last_request.env['rack.session']['omniauth.auth']).to eq(auth_hash)
+      end
+
+      it 'should return a 200 with an XML object of user information on success' do
+        set_app!({
+          cas_server:          'https://example.org:3000',
+          authentication_path: '/dn',
+          format_header:       'application/xml',
+          format:              'xml',
+          dnc_options: { transformation: 'downcase' },
+          ssl_config:  {
+            ca_file:     'spec/certs/CA.pem',
+            client_cert: 'spec/certs/client.pem',
+            client_key:  'spec/certs/key.np.pem'
+          }
+        })
+        stub_request(:get, "https://example.org:3000/dn/cn=ruby%20certificate%20rbcert,dc=ruby-lang,dc=org/info.xml?issuerDN=cn=ruby%20ca,dc=ruby-lang,dc=org").
+        with(:headers => {'Accept'=>'application/xml', 'Content-Type'=>'application/xml', 'Host'=>'example.org:3000', 'User-Agent'=>'Faraday via Ruby 2.1.5', 'X-Xsrf-Useprotection'=>'false'}).
+        to_return(status: 200, body: valid_user_xml, headers: {})
+
+        header 'Ssl-Client-Cert', user_cert
+        get '/auth/dice'
+        follow_redirect!
+        expect(last_response.location).to eq('/')
+        raw_info = last_request.env['rack.session']['omniauth.auth']['extra']['raw_info']
+        expect(raw_info).to eq(valid_user_xml)
       end
     end
 
     context 'fail' do
-      pending 'todo'
+      it 'should raise a 404 with text for a non-existent user DN' do
+        stub_request(:get, "https://example.org:3000/dn/cn=ruby%20certificate%20rbcert,dc=ruby-lang,dc=org/info.json?issuerDN=cn=ruby%20ca,dc=ruby-lang,dc=org").
+        with(:headers => {'Accept'=>'application/json', 'Content-Type'=>'application/json', 'Host'=>'example.org:3000', 'User-Agent'=>'Faraday via Ruby 2.1.5', 'X-Xsrf-Useprotection'=>'false'}).
+        to_return(status: 404, body: "User of dn:cn=ruby certificate rbcert,dc=ruby-lang,dc=org not found", headers: {})
+
+        header 'Ssl-Client-Cert', user_cert
+        get '/auth/dice'
+        expect { get '/auth/dice'; follow_redirect! }.to raise_error(OmniAuth::Error, 'invalid_credentials')
+      end
     end
   end
-
-#    expect(last_request.env['rack.session'][:dice][:dn]).to eq(cert_dn)
-#    expect(last_request.env['rack.session'][:dice][:issuer]).to eq(cert_issuer)
-#    expect(last_request.env['rack.session'][:dice][:sid]).to eq(cert_sid)
-#    expect(last_response.status).to eq(200)
-
 end
--- spec/omniauth/strategies/dice_spec.rb
+++ spec/omniauth/strategies/dice_spec.rb
@@ -33,7 +33,6 @@ describe OmniAuth::Strategies::Dice do
     end
 
     it "should return the default options" do
-      expect(subject.options.uid_field).to     eq(:dn)
       expect(subject.options.format).to        eq('json')
       expect(subject.options.format_header).to eq('application/json')
     end
@@ -62,39 +61,4 @@ describe OmniAuth::Strategies::Dice do
       expect(formatted_reverse_client_dn).to eq(formatted_client_dn)
     end
   end
-
-  context "Old specs" do
-    context "success" do
-      before :each do
-        pending
-      end
-
-      it "should return dn from raw_info if available" do
-        subject.stub!(:raw_info).and_return({'dn' => 'givenName = Steven Haddox, ou = apache, ou = org'})
-        subject.dn.should eq('givenName = Steven Haddox, ou = apache, ou = org')
-      end
-
-      it "should return email from raw_info if available" do
-        subject.stub!(:raw_info).and_return({'email' => 'stevenhaddox@shortmail.com'})
-        subject.email.should eq('stevenhaddox@shortmail.com')
-      end
-
-      it "should return nil if there is no raw_info and email access is not allowed" do
-        subject.stub!(:raw_info).and_return({})
-        subject.email.should be_nil
-      end
-
-      it "should return the first email if there is no raw_info and email access is allowed" do
-        subject.stub!(:raw_info).and_return({})
-        subject.options['scope'] = 'user'
-        subject.stub!(:emails).and_return([ 'you@example.com' ])
-        subject.email.should eq('you@example.com')
-      end
-    end
-
-    context "failure" do
-      pending
-    end
-  end
-
 end
