0a99539c9be2cd9ae85a17604a9dd755fac7e2f1
David G. Daniel <david.daniel@bia-boeing.com>
2017-06-14 10:48:13 -0400

    Initial Commit with fixes for ruby < 2.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
+require "simplecov"
+SimpleCov.start
+
 require 'rubygems'
 require 'bundler'
 Bundler.setup :default, :development, :test
